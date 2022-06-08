913f8fc670d1ba72537784ea1c7e29c9eb7f6067
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2022-06-08 02:53:33 +0200

    completed custom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import Card from "./Card";
+import Contacts from "./contacts";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Card
+        name={Contacts[0].name}
+        img={Contacts[0].imgURL}
+        tel={Contacts[0].phone}
+        email={Contacts[0].email}
+      />
+
+      <Card
+        name={Contacts[1].name}
+        img={Contacts[1].imgURL}
+        tel={Contacts[1].phone}
+        email={Contacts[1].email}
+      />
+
+      <Card
+        name={Contacts[2].name}
+        img={Contacts[2].imgURL}
+        tel={Contacts[2].phone}
+        email={Contacts[2].email}
+      />
     </div>
   );
 }
